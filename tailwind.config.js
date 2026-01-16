3998289b596d8eb4afc1aa8f7d516d3c709a0176
Sarim Ali <sarimali8549@gmail.com>
2026-01-16 15:51:07 +0530

    Upgrade UI to Tailwind CSS v3 with Glassmorphism and fix reservation form

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          500: '#d4af37',
+          600: '#b38b2d',
+        },
+        beige: {
+          500: '#f5f5dc',
+          600: '#dcdcdc',
+          800: '#8b8b83',
+        }
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Roboto"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
